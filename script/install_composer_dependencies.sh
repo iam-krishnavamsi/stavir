f6ccec6354ca080f45afd9b8330319f2237800f7
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2023-09-20 12:54:06 +0530

    Update install_composer_dependencies.sh

--- script/install_composer_dependencies.sh
+++ script/install_composer_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/html
 yum install wget -y
-wget https://getcomposer.org/composer.json
-//php composer.phar install
+php composer.phar install
+wget https://getcomposer.org/composer.phar
