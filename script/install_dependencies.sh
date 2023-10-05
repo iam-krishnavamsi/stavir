f630788af5a00fbc193b16208730b359abb70187
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2023-10-05 12:26:25 +0530

    Update install_dependencies.sh

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Check if Apache (httpd) is installed; if not, install it
-if ! [ -x "$(command -v apache2)" ]; then
-    sudo apt-get update
-    sudo apt-get install apache2 -y
-fi
+#!/bin/bash
+
+if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd24 >&2;   exit 1; fi # install apache if not already installed
+if ! [ -x "$(command -v php)" ]; then yum install php72 php72-mysqlnd php72-imap php72-pecl-memcache php72-pecl-apcu php72-gd php72-mbstring -y >&2;   exit 1; fi # install PHP
